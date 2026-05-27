b7f9ba2676fe24fa7f8f01d8395046cb84c14664
wiltontechdev <wilton.techdev@gmail.com>
2026-05-27 07:58:39 +0300

    Testing works

--- target/debug/deps/Learn.d
+++ target/debug/deps/Learn.d
@@ -1,6 +1,10 @@
-C:\Users\HomePC\Desktop\crytpo\Intro-to-rust\Learning\Learn\target\debug\deps\Learn.d: src\main.rs src\household.rs
+C:\Users\HomePC\Desktop\crytpo\Intro-to-rust\Learning\Learn\target\debug\deps\Learn.d: src\main.rs src\household.rs src\inventory.rs src\distribution.rs src\report.rs src\menus.rs
 
-C:\Users\HomePC\Desktop\crytpo\Intro-to-rust\Learning\Learn\target\debug\deps\Learn.exe: src\main.rs src\household.rs
+C:\Users\HomePC\Desktop\crytpo\Intro-to-rust\Learning\Learn\target\debug\deps\Learn.exe: src\main.rs src\household.rs src\inventory.rs src\distribution.rs src\report.rs src\menus.rs
 
 src\main.rs:
 src\household.rs:
+src\inventory.rs:
+src\distribution.rs:
+src\report.rs:
+src\menus.rs:
